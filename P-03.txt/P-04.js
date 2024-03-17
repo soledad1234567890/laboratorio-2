30903d3bd5da2105dc7a1d964bc4d3f8c8c82613
soledad1234567890 <corosoledad66@gmail.com>
2024-03-16 21:01:26 -0400

    first commit

---
+++ P-03.txt/P-04.js
@@ -0,0 +1,5 @@
+var person1 = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30
+};
---
+++ P-03.txt/P-05.js
@@ -0,0 +1,15 @@
+const emailList = [
+    "javascript@gmail.com", 
+    "abc.com",
+    "mydomain.com",
+    "Python@hotmail.com",
+    "test",
+    "anotherEmail@kenkoo.com",
+    "typescript@gmail.com",
+    "mailOfPikachu@@@@@",
+    "elta@kkk.co",
+    "user123.....",
+    "wrongEmail@gmail.com",
+    "kattis.go",
+    "java@gamil.com",
+  ];
